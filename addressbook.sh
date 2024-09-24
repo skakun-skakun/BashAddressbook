0ff0c9775ea4bb3cf5b64e7ba94a531a353cc89e
skakun-skakun <o.skakun@kse.org.ua>
2024-09-24 22:43:53 +0300

    Add files via upload

--- addressbook.sh
+++ addressbook.sh
@@ -27,18 +27,31 @@ case $action in
     done < db.txt
     ;;
   remove)
+    new_text=""
+    broken=false
     echo "Enter a string that can identify a user (name/phone/email)"
     read val
     IFS=';'
     while read -ra line; do
+      if [[ -z "$line" ]]
+      then
+        continue
+      fi
       for i in "${line[@]}"; do
         if [[ "$i" == "$val" ]]
         then
-          
+          broken=true
           break
         fi
       done
+      if [[ "$broken" = false ]]
+      then
+        new_text+="${line[0]};${line[1]};${line[2]}
+"
+      fi
+      broken=false
     done < db.txt
+    echo "${new_text}" > db.txt
     ;;
   *)
     ;;
